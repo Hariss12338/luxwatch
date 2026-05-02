a86aeff4fa098466c3bd1434e3f04d1df3cee0b7
Haris khan <hk91527388@gmail.com>
2026-05-03 01:52:04 +0530

    fix base path properly

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['three', '@react-three/fiber', '@react-three/drei', 'gsap', 'lenis']
-  }
+  base: './'
 })
